da894c50e60322e6569d92d4263e6a851e5dcef9
Jeffrey Johnson <jeffcjohnson@live.com>
2015-08-18 21:38:38 -0700

    Switch to Angular

--- main.js
+++ main.js
@@ -1,102 +1,83 @@
-var axisDown = false;
-var buttonDown = false;
-function inputLoop() {
-    var gamepads = navigator.getGamepads();
-    for (var playerIndex = 0; playerIndex < gamepads.length; playerIndex++) {
-        if(!document.hasFocus()) break;
-        var gamepad = gamepads[playerIndex];
-        if (gamepad) {
-            if(gamepad.axes[0] >= 0.9 && !axisDown) {
-                document.dispatchEvent(new Event('right'));
-                axisDown = true;
-            }
-            if(gamepad.axes[0] <= -0.9 && !axisDown) {
-                document.dispatchEvent(new Event('left'));
-                axisDown = true;
-            }
-            else if(gamepad.axes[1] >= 0.9 && !axisDown) {
-                document.dispatchEvent(new Event('down'));
-                axisDown = true;
-            }
-            else if(gamepad.axes[1] <= -0.9 && !axisDown) {
-                document.dispatchEvent(new Event('up'));
-                axisDown = true;
-            }
-            else if(Math.abs(gamepad.axes[0]) < 0.9 && Math.abs(gamepad.axes[1]) < 0.9) {
-                axisDown = false;
-            }
-            if(gamepad.buttons[0].pressed == true && !buttonDown) {
-                document.dispatchEvent(new Event('select'));
-                buttonDown = true;
-            }
-            else if(gamepad.buttons[1].pressed == true && !buttonDown) {
-                document.dispatchEvent(new Event('back'));
-                buttonDown = true;
-            }
-            else if(gamepad.buttons[0].pressed == false && gamepad.buttons[1].pressed == false) {
-                buttonDown = false;
-            }
-        }
+angular.module('arcadeCabinet', [])
+.controller('arcadeCtrl', function($scope, $http) {
+    $scope.selectedGame = 0;
+    $scope.games = [];
+
+    var gameNames = ["Distilled", "Closing", "Closing", "Closing", "Closing"];
+    gameNames.forEach(function(name) {
+        $http.get("Games/" + name + "/Game.json")
+        .success(function(data) {
+            $scope.games.push(data);
+        });
+    });
+
+    var gameDiv = document.getElementById("games");
+
+    $scope.mod = function(num) {
+        var base = $scope.games.length;
+        return ((num % base) + base) % base;
     }
-    window.requestAnimationFrame(inputLoop);
-}
-inputLoop();
 
-var gameDiv;
+    function select(index) {
+        $scope.selectedGame = index;
+        $scope.$apply();
+    }
 
-var first = true;
-function loadGame(name) {
-    var game;
-    var http_request = new XMLHttpRequest();
-    http_request.open("GET", "Games/"+name+"/Game.json", true);
-    http_request.onreadystatechange = function () {
-        if (http_request.readyState === 4) {
-            game = JSON.parse(http_request.responseText);
-            var gameHtml = "<div class='game " + (first ? "selected" : "") + "'>" +
-                "<a href='game:Games\\" + name + "\\" + game.Command + "'>" +
-                "<h2 class='title'>" + game.Name + "</h2>" +
-                "<img src='Games/" + name + "/" + game.Image + "' />" +
-                "<p class='description'>" + game.Description + "</p></a></div>";
-            gameDiv.innerHTML += gameHtml;
-            first = false;
-            updateCarousel();
-        }
-    };
-    http_request.send(null);
-}
-
-var games = ["Distilled", "Closing", "Closing", "Closing", "Closing"];
-
-function mod(num, base) {
-    return ((num % base) + base) % base;
-}
-
-var selectedGame = 0;
-function select(index) {
-    var count = gameDiv.children.length;
-    gameDiv.getElementsByClassName("game")[mod(selectedGame, count)].classList.remove("selected");
-    selectedGame = index;
-    gameDiv.getElementsByClassName("game")[mod(selectedGame, count)].classList.add("selected");
-    updateCarousel();
-}
-
-function updateCarousel() {
-    var count = gameDiv.children.length;
-    for(var i = 0; i < count; i++) {
+    $scope.getTransform = function(i) {
+        var count = $scope.games.length;
         var z = Math.round((1000 / 2) / Math.tan(Math.PI / count));
-        gameDiv.children[i].style['transform'] = "rotateY(" + (i - selectedGame) * 360 / count + "deg) translateZ(" + z + "px)";
+        return "rotateY(" + (i - $scope.selectedGame) * 360 / count + "deg) translateZ(" + z + "px)";
     }
-}
 
-document.addEventListener('left', function (e) { select(selectedGame - 1); }, false);
-document.addEventListener('right', function (e) { select(selectedGame + 1); }, false);
-document.addEventListener('up', function (e) { select(selectedGame - 1); }, false);
-document.addEventListener('down', function (e) { select(selectedGame + 1); }, false);
-document.addEventListener('select', function (e) { gameDiv.getElementsByClassName("game")[mod(selectedGame, gameDiv.children.length)].children[0].click() }, false);
-document.addEventListener('back', function (e) { console.log("back"); }, false);
+    var axisDown = false;
+    var buttonDown = false;
+    function inputLoop() {
+        var gamepads = navigator.getGamepads();
+        for (var playerIndex = 0; playerIndex < gamepads.length; playerIndex++) {
+            if(!document.hasFocus()) break;
+            var gamepad = gamepads[playerIndex];
+            if (gamepad) {
+                if(gamepad.axes[0] >= 0.9 && !axisDown) {
+                    $scope.$broadcast('right');
+                    axisDown = true;
+                }
+                if(gamepad.axes[0] <= -0.9 && !axisDown) {
+                    $scope.$broadcast('left');
+                    axisDown = true;
+                }
+                else if(gamepad.axes[1] >= 0.9 && !axisDown) {
+                    $scope.$broadcast('down');
+                    axisDown = true;
+                }
+                else if(gamepad.axes[1] <= -0.9 && !axisDown) {
+                    $scope.$broadcast('up');
+                    axisDown = true;
+                }
+                else if(Math.abs(gamepad.axes[0]) < 0.9 && Math.abs(gamepad.axes[1]) < 0.9) {
+                    axisDown = false;
+                }
+                if(gamepad.buttons[0].pressed == true && !buttonDown) {
+                    $scope.$broadcast('select');
+                    buttonDown = true;
+                }
+                else if(gamepad.buttons[1].pressed == true && !buttonDown) {
+                    $scope.$broadcast('back');
+                    buttonDown = true;
+                }
+                else if(gamepad.buttons[0].pressed == false && gamepad.buttons[1].pressed == false) {
+                    buttonDown = false;
+                }
+            }
+        }
+        window.requestAnimationFrame(inputLoop);
+    }
+    inputLoop();
 
-
-document.addEventListener("DOMContentLoaded", function (e) {
-    gameDiv = document.getElementById("games");
-    games.forEach(loadGame);
+    $scope.$on('right', function (e) { select($scope.selectedGame + 1); });
+    $scope.$on('left', function (e) { select($scope.selectedGame - 1); });
+    $scope.$on('up', function (e) { select($scope.selectedGame - 1); });
+    $scope.$on('down', function (e) { select($scope.selectedGame + 1); });
+    $scope.$on('select', function (e) { gameDiv.getElementsByClassName("game")[$scope.mod($scope.selectedGame, gameDiv.children.length)].children[0].click() });
+    $scope.$on('back', function (e) { console.log("back"); });
 });
+
